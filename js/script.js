4c0fe9b417aa2c5c25e3b2b9a7fa1c0f4a52c4c2
Sophia M Banda <51414936+sophiabanda@users.noreply.github.com>
2022-12-07 16:57:53 -0500

    experimented with loops and conditionals

---
+++ js/script.js
@@ -0,0 +1,77 @@
+
+// let dayOfTheWeek = 'sunday';
+
+// if (dayOfTheWeek === 'sunday' || dayOfTheWeek === 'saturday') {
+//     console.log(`It's the weeeekeeeennnnddd!`)
+// } else {
+//     console.log(`Boooo, have to go to work... 😞`)
+// }
+
+// let age = 20;
+// let result = age > 18 ? 'Is not a child.' : 'Is still a child.'
+
+// console.log(result)
+
+// for (let i = 1; i < 101; i++) {
+//     console.log(i)
+// }
+
+// let ages = [20, 30, 25, 22, 31];
+// for (let i = 0; i < ages.length; i++) {
+//     console.log(`I am ${ages[i] - 2} years old today`)
+// }
+
+// let personAge = [ 
+//     {name: 'Person1', age: 26}, 
+//     {name: 'Person2', age: 12}, 
+//     {name: 'Person3', age: 39},
+
+// ];
+
+// for (let i = 0; i < personAge.length; i ++) {
+//     if (personAge[i].age < 19 && personAge[i].age > 13) {
+//     console.log(personAge[i].name + " is a teenager.");
+// } else if (personAge[i].age < 13) {
+//     console.log(personAge[i].name + " is a child.");
+// } else {
+//     console.log(personAge[i].name + " is an adult.");
+// }
+// }
+
+// let i = 1;
+
+// while (i < 5) {
+//     console.log(i);
+//     i ++;
+// }
+
+// let ia = 1;
+
+// do {
+//     console.log(ia);
+//     ia ++;
+// }
+//   while (ia < 5)
+
+// let fruits = ['apple', 'orange', 'grape'];
+// let text = '';
+// let i = 0;
+
+// while (fruits[i]) {
+//     text = text + ' ' + fruits[i];
+//     i ++;;
+// }
+
+// console.log(text)
+
+// Have never seen this before in my life: ************************************
+// let fruits = ['apple', 'orange', 'grape'];
+// let text = '';
+// let i = 0;
+
+// for (;fruits[i];) {
+//     text = text + ' ' + fruits[i];
+//     i ++;
+// }
+
+// console.log(text)
