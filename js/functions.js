3954794fb44ee4950ae1267f3225a59237c7a703
Sophia M Banda <51414936+sophiabanda@users.noreply.github.com>
2022-12-11 12:15:35 -0500

    more functions

--- js/functions.js
+++ js/functions.js
@@ -70,32 +70,40 @@
 // console.log(age); // returns  10
 
 
-function dividerTool (divider, divisor) {
-  if (divider === 0 || divisor === 0) {
-    console.log('This tool does not work with 0 as an input. Try another number.')
-  } else {
-    let result = divider/divisor;
-    return result;
-  }
+// function dividerTool (divider, divisor) {
+//   if (divider === 0 || divisor === 0) {
+//     return 'This tool does not work with 0 as an input. Try another number.';
+//   } else {
+//     let result = divider/divisor;
+//     return result;
+//   }
+// }
+
+// console.log(dividerTool(176, 2));
+// console.log(dividerTool(1200, 4));
+// console.log(dividerTool(10, 0)); //Why does this give me the console.log but also the undefined? Is it undefined for result?????????
+
+
+// // a function without a return statement
+// function add(number1, number2) {
+//   console.log(number1 + number2);
+// }
+// let result2 = add(1, 2);
+// console.log(result2) //will console.log undefined since result2 is assigned what is returned by add(1, 2) which is undefined. 
+
+// // a function with a return statement
+// function multiply(number1, number2) {
+//   return number1 * number2;
+// }
+// let result = multiply(3, 5);
+// console.log(result);
+
+//PREDEFINED FUNCTIONS ------------------------------------
+console.log('Hello!');
+console.warn('Warning!');
+console.error('Error!');
+
+function runThisLater() {
+  console.log('Hello!');
 }
-
-console.log(dividerTool(176, 2));
-console.log(dividerTool(1200, 4));
-console.log(dividerTool(10, 0)); //Why does this give me the console.log but also the undefined? Is it undefined for result?
-
-
-// a function without a return statement
-function add(number1, number2) {
-  console.log(number1 + number2);
-}
-let result2 = add(1, 2);
-console.log(result2) //why does console.log undefined? 
-
-// a function with a return statement
-function multiply(number1, number2) {
-  return number1 * number2;
-}
-let result = multiply(3, 5);
-console.log(result);
-
-//I AM COFUSED AS TO HOW CONSOLE.LOG IS WORKING IN THESE TWO INSTANCES. 
+setTimeout(runThisLater, 1000); // Run this in 1000ms
