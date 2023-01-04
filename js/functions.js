2061f8cc1848807187bd0ebdb17b99b55a546da2
Sophia M Banda <51414936+sophiabanda@users.noreply.github.com>
2023-01-04 14:24:22 -0500

    commented out functions

--- js/functions.js
+++ js/functions.js
@@ -70,35 +70,34 @@
 // console.log(age); // returns  10
 
 
-function dividerTool (divider, divisor) {
-  if (divider === 0) {
-    return 'This tool does not work with 0 as an input. Try another number.';
-  } else {
-    let result = divider/divisor;
-    return result;
+// function dividerTool (divider, divisor) {
+//   if (divider === 0) {
+//     return 'This tool does not work with 0 as an input. Try another number.';
+//   } else {
+//     let result = divider/divisor;
+//     return result;
     
-  }
-}
+//   }
+// }
 
-console.log(dividerTool(176, 2));
-console.log(dividerTool(1200, 4));
-console.log(dividerTool(100, 0)); //Why does this give me the console.log but also the undefined? Is it undefined for result?
+// console.log(dividerTool(176, 2));
+// console.log(dividerTool(1200, 4));
+// console.log(dividerTool(100, 0)); 
 
 
 // a function without a return statement
-function add(number1, number2) {
-  return number1 + number2;
-  console.log(number1 + number2);
-}
-let result2 = add(1, 2);
-console.log(result2) //will console.log undefined since result2 is assigned what is returned by add(1, 2) which is undefined. 
+// function add(number1, number2) {
+//   return number1 + number2;
+// }
+// let result2 = add(1, 2);
+// console.log(result2) //will console.log undefined since result2 is assigned what is returned by add(1, 2) which is undefined. 
 
-// a function with a return statement
-function multiply(number1, number2) {
-  return number1 * number2;
-}
-let result = multiply(3, 5);
-console.log(result);
+// // a function with a return statement
+// function multiply(number1, number2) {
+//   return number1 * number2;
+// }
+// let result = multiply(3, 5);
+// console.log(result);
 
 // //PREDEFINED FUNCTIONS ------------------------------------
 // console.log('Hello!');
@@ -109,3 +108,4 @@ console.log(result);
 //   console.log('Hello!');
 // }
 // setTimeout(runThisLater, 1000); // Run this in 1000ms
+
